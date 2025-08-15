feed4a89406f98855a66816f2516251fa5852355
Ibrahim Alshekh <ibrahim.shekh.m@gmail.com>
2025-08-15 20:14:11 +0200

    remove unused 'welcome' route from web.php for cleaner routing setup

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('welcome', function () {
-    echo config('inertia.ssr.url');
-    $response = Inertia::render('welcome');
-    dd($response);
-    return 'test';
-});
-
 Route::get('/', function () {
     return Inertia::render('home');
 })->name('home');
